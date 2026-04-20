298f01383cfc7ce112d4176199d9fc10505557ac
Javier Esteban <javierestebansegura98@gmail.com>
2026-04-20 12:27:09 +0100

    Configured BrowserRouter in main.jsx

---
+++ src/pages/AnalisisZona.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnalisisZona = () => {
+    return (
+        <div>
+            <h2>Vista general (Aquí irán las gráficas y KPIs)</h2>
+        </div>
+    );
+};
+
+export default AnalisisZona;
